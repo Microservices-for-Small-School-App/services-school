9e14e9f07d01489c61412caaddc15918f40e7136
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-20 08:05:52 +0530

    Code Refactoring

---
+++ Source/School.Business/DefaultResponseBusiness.cs
@@ -0,0 +1,19 @@
+﻿using School.Data.Dtos;
+
+namespace School.Business
+{
+
+    public static class DefaultResponseBusiness
+    {
+        public static ApiResponseDto<string> SendDefaultApiEndpointOutput()
+        {
+            return ApiResponseDto<string>.Create("Welcome to Minimal API Endpoint");
+        }
+
+        public static ApiResponseDto<string> SendDefaultApiEndpointV1Output()
+        {
+            return ApiResponseDto<string>.Create("Welcome to Minimal API Endpoint V1");
+        }
+    }
+
+}
---
+++ Source/School.Data/Dtos/ApiResponseDto.cs
@@ -0,0 +1,23 @@
+﻿namespace School.Data.Dtos
+{
+
+    public record ApiResponseDto<T>
+    {
+        public bool Success { get; set; }
+
+        public string? Message { get; set; }
+
+        public T? Data { get; set; }
+
+        public static ApiResponseDto<T> Create(T? data = default, string message = "Success", bool success = true)
+        {
+            return new()
+            {
+                Success = success,
+                Message = message,
+                Data = data
+            };
+        }
+    }
+
+}
