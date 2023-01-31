2e5147bc6656d229b4b00d08690608188f2675a8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-31 12:24:22 +0530

    Db Context and Seed Data

---
+++ Source/School.Api/Persistence/SchoolDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.Api.Data.Entities;
+using School.Api.Persistence.SeedData;
+
+namespace School.Api.Persistence
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses => Set<Course>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new CourseData());
+        }
+    }
+}
