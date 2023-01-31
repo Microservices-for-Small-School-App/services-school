e3db56d8e721ccd826828ac67e20e31046189bf4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-31 12:16:55 +0530

    Entities

---
+++ Source/School.Api/Data/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿namespace School.Api.Data.Entities
+{
+
+    public class Course : BaseEntity
+    {
+        public string? CourseId { get; set; }
+
+        public string? Name { get; set; }
+
+        // Duration in Weeks
+        public int Duration { get; set; }
+
+        public string? Description { get; set; }
+    }
+
+}
