0ef4395d2e36b60fdd7d1f2ef676bffa81c38320
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-13 20:20:16 +0530

    Code Refactoring

--- Source/School.Api/ApplicationCore/Common/Constants.cs
+++ Source/School.Api/ApplicationCore/Common/Constants.cs
@@ -1,41 +1,38 @@
-﻿namespace School.Api.ApplicationCore.Common
+﻿namespace School.Api.ApplicationCore.Common;
+
+public static class Constants
 {
-
-    public static class Constants
+    public static class HelloWorldRoutes
     {
-        public static class HelloWorldRoutes
-        {
-            public static string Root { get; } = "/";
+        public static string Root { get; } = "/";
 
-            public static string HelloWorld { get; } = "/hw";
+        public static string HelloWorld { get; } = "/hw";
 
-            public static string Api { get; } = "/api";
+        public static string Api { get; } = "/api";
 
-            public static string ApiV1 { get; } = "/api/v1";
-        }
-
-        public static class UsersRoutes
-        {
-            public static string Prefix { get; } = "/api/users";
-
-            public static string Root { get; } = "/";
-
-            public static string ActionById { get; } = "/{id}";
-        }
-
-        public static class CoursesRoutes
-        {
-            public static string Prefix { get; } = "/api/courses";
-
-            public static string Root { get; } = "/";
-
-            public static string ActionById { get; } = "/api/courses/{Id}";
-        }
-
-        public static class InMemoryDatabase
-        {
-            public static string Name { get; } = "SchoolDatabase";
-        }
+        public static string ApiV1 { get; } = "/api/v1";
     }
 
+    public static class UsersRoutes
+    {
+        public static string Prefix { get; } = "/api/users";
+
+        public static string Root { get; } = "/";
+
+        public static string ActionById { get; } = "/{id}";
+    }
+
+    public static class CoursesRoutes
+    {
+        public static string Prefix { get; } = "/api/courses";
+
+        public static string Root { get; } = "/";
+
+        public static string ActionById { get; } = "/api/courses/{Id}";
+    }
+
+    public static class InMemoryDatabase
+    {
+        public static string Name { get; } = "SchoolDatabase";
+    }
 }
