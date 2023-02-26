0df80f1cdd1c48d2359aa274ebc7efa18db5b187
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-26 10:53:02 +0530

    configured Http Pipeline

--- Source/School.Api/Program.cs
+++ Source/School.Api/Program.cs
@@ -1,28 +1,13 @@
-using School.Api.Endpoints;
 using School.Api.Extensions;
-using School.Api.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add the Services
+// Add services to the container.
 _ = builder.Services.ConfigureDependedServices();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-    // TODO: To be removed once we have .sqlproj
-    using var scope = app.Services.CreateScope();
-    using var context = scope.ServiceProvider.GetService<SchoolDbContext>();
-    _ = (context?.Database.EnsureCreated());
-}
-
-// Endpoints
-app.MapHelloWorldEndpoints();
-app.MapUserEndpoints();
-app.MapCourseEndpoints();
+// Configure the HTTP request pipeline.
+app.ConfigureHttpRequestPipeline();
 
 app.Run();
