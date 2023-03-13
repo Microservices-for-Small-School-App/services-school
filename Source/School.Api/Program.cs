e53e29170945612ac4c5e676e0cfb280489ab219
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-13 22:28:46 +0530

    Update Program.cs

--- Source/School.Api/Program.cs
+++ Source/School.Api/Program.cs
@@ -5,11 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var logger = new LoggerConfiguration()
-    .WriteTo.Debug()
-    .WriteTo.Console()
-    .ReadFrom.Configuration(builder.Configuration)
-    .Enrich.FromLogContext()
-    .CreateLogger();
+                    .WriteTo.Debug()
+                    .WriteTo.Console()
+                    .ReadFrom.Configuration(builder.Configuration)
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
 
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger);
